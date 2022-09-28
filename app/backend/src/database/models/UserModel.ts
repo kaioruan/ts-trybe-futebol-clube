1ec9b211c8a305b16fb7d87b2f6cb1ff546003e1
kaioruan <kaio.ruan@souunit.com.br>
2022-09-28 15:59:46 -0300

    feat: create database user

---
+++ app/backend/src/database/models/UserModel.ts
@@ -0,0 +1,52 @@
+import { DataTypes, Model } from 'sequelize/types';
+import db from '.';
+
+class User extends Model {
+  id!: number;
+  username!: string;
+  email!: string;
+  password!: string;
+  role!: string;
+}
+
+User.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    field: 'created_at',
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    field: 'updated_at',
+  },
+}, {
+  sequelize: db,
+  modelName: 'users',
+  // underscored: true,
+  timestamps: false,
+});
+
+export default User;
