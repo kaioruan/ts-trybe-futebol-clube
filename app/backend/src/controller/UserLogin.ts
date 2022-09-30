eea7340beb0f48d37a30daacfc4bb144f8f76d89
kaioruan <kaio.ruan@souunit.com.br>
2022-09-30 12:42:03 -0300

    feat: create test route login

--- app/backend/src/controller/UserLogin.ts
+++ app/backend/src/controller/UserLogin.ts
@@ -9,19 +9,13 @@ class LoginController {
   public login = async (req: Request, res: Response): Promise<Response> => {
     const { email, password } = req.body;
     const result = await this.loginService.login(email, password);
-    if (!result) {
-      return res.status(404).json({ message: 'User not found' });
-    }
     return res.status(200).json({ token: result });
   };
 
   public validate = async (req: Request, res: Response): Promise<Response> => {
     const { authorization } = req.headers;
-    if (authorization) {
-      const result = await this.loginService.validate(authorization);
-      return res.status(200).json({ role: result });
-    }
-    return res.status(401).json({ message: 'Unauthorized' });
+    const result = await this.loginService.validate(authorization as string);
+    return res.status(200).json({ role: result });
   };
 }
 
---
+++ app/backend/src/tests/Login.test.ts
@@ -0,0 +1,56 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+import { app } from '../app';
+import User from '../database/models/UserModel';
+import { UsersMock, token } from './mocks';
+
+chai.use(chaiHttp);
+
+const expect = chai.expect;
+
+const UserLogin = {
+  email: 'divinesmite@critic.com',
+  password: 'secret_admin',
+}
+const invalidLogin = {
+  email: 'usuario_suspeito@gmail.com',
+  password: 'secret_admin',
+}
+
+describe('Rota Login', () => {
+  describe('Usuário válido', () => {
+    before(async () => {
+      sinon.stub(User, 'findOne').resolves(UsersMock[0] as User);
+    })
+
+    after(() => {
+      (User.findOne as sinon.SinonStub).restore();
+    })
+    it('Retorna um Token JWT com status 200', async () => {
+      const response = await chai.request(app)
+        .post('/login')
+        .send(UserLogin);
+
+      expect(response.body).to.have.property('token');
+      expect(response.status).to.be.equal(200);
+    });
+  })
+  describe('Rota /login/validate', () => {
+    before(async () => {
+      sinon.stub(User, 'findOne').resolves(UsersMock[0] as User);
+    })
+
+    after(() => {
+      (User.findOne as sinon.SinonStub).restore();
+    })
+    it('Retornar usuário encontrado', async () => {
+      const response = await chai.request(app)
+        .get('/login/validate')
+        .set('authorization', token);
+        expect(response.body).to.be.deep.equal({ role: UsersMock[0].role });
+        expect(response.status).to.be.equal(200);
+    })
+  })
+})
